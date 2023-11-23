60aa22a42bf8d0a65221f46bd828efc81799bc5f
keyurpatel <keyurpatel8288@gmail.com>
2023-11-23 14:39:21 -0500

    h

--- frontend.sh
+++ frontend.sh
@@ -11,6 +11,7 @@ echo downling the the Files
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tmp/expense.log
 
 cd /usr/share/nginx/html
+
 unzip /tmp/frontend.zip >/tmp/expense.log
 
 echo Starting Nginx service
