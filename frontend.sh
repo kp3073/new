5e48c8c60203d99338eeb225d5e466a0c87187c0
keyurpatel <keyurpatel8288@gmail.com>
2023-11-23 14:44:27 -0500

    h

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 echo installing Nginx
-dnf install nginx -y >/tmp/expense.log
+dnf install nginx -y >>/tmp/expense.log
 
 
-cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >>/tmp/expense.log
 
 echo Removing Old contant of Nginx
-rm -rf /usr/share/nginx/html/* >/tmp/expense.log
+rm -rf /usr/share/nginx/html/* >>/tmp/expense.log
 
 echo downling the the Files
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/expense.log
 
 cd /usr/share/nginx/html
 
-unzip /tmp/frontend.zip >/tmp/expense.log
+unzip /tmp/frontend.zip >>/tmp/expense.log
 
 echo Starting Nginx service
-systemctl enable nginx  >/tmp/expense.log
-systemctl restart nginx >/tmp/expense.log
+systemctl enable nginx  >>/tmp/expense.log
+systemctl restart nginx >>/tmp/expense.log
